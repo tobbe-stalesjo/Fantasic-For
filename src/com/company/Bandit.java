b30133a30743d734e91cc4c7170f700f3526411f
florind <florind.haliti@gmail.com>
2019-12-04 10:51:59 +0100

    Fixing details

--- src/com/company/Bandit.java
+++ src/com/company/Bandit.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Bandit extends  Monster {
 
-    Potion potion = new Potion();
+    Potion potion = new Potion("healing potion", 1, 0, 0.5);
 
     public Bandit(int x, int y) {
 

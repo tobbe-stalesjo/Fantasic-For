d3a0a9b5945893850b952e54c643c288e055f464
Lisa <luyao2014@gmail.com>
2019-12-13 11:20:56 +0100

    boolean defeated Monster

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -6,18 +6,18 @@ public class Hero extends Creature {
 
     Backpack backpack = new Backpack();
     private boolean injured;
-    Spider spider = new Spider();
-    Menu menu = new Menu();
-    Maze maze = new Maze();
+    Maze maze;
 
-    public Hero(int startRoom, boolean injured) {
-        super(startRoom);
+    public Hero(int startRoom, String name, boolean injured, Maze maze) {
+        super(startRoom, name);
         this.injured = injured;
+        this.maze = maze;
+
     }
 
     private void attackOrRunAway(){
         System.out.println("You've encountered an enemy. Do you want to attack or run away?");
-        
+
     }
 /*
     private void drinkPotion(){
@@ -42,60 +42,61 @@ public class Hero extends Creature {
         if (backpack.getItemName("superDagger") != null) {
             if (backpack.getItemName("superDagger").equals("superDagger")) {
                 System.out.println("You defeat the Spider! Continue your adventure and find other Monster!");
+                maze.removeSpiderFromMaze();
+
+
+
             }
         } else {
             System.out.println("Warning! You are injured. Drink a portion to heal. You need weapon to kill Monster!");
         }
     }
 
-    public boolean isSpiderDefeated(){
-        if (backpack.getItemName("superDagger") != null) {
-            if (backpack.getItemName("superDagger").equals("superDagger")) {
-                System.out.println(" ");
+    public boolean isSpiderDefeated(){//search name
+        Spider spider = (Spider)maze.getMonster("Spider");
 
-            }
+        if(spider == null){
             return true;
-        } else {
-            System.out.println(" ");
+
         }
         return false;
-
     }
 
 
     public void attackBandit(){
         if(isSpiderDefeated()){
             System.out.println("You defeat the Bandit! Continue your adventure and find other Monster!");
+            maze.removeBanditFromMaze();
         }
-     else {
-        System.out.println("You need defeat Spider first!");
+        else {
+            System.out.println("You need defeat Spider first!");
+        }
     }
-}
 
     public boolean isBanditDefeated() {
 
-        if (isSpiderDefeated()) {
+        Bandit bandit = (Bandit)maze.getMonster("Bandit");
 
+        if(bandit == null){
             return true;
-        } else {
-            System.out.println(" ");
+
         }
         return false;
-
     }
 
 
 
-   public void attackDragonBoss(){
-            if (isBanditDefeated()) {
-                System.out.println("Congratulations! You defeat the DragonBoss! You win the game!");
-            } else {
-                System.out.println("You need defeat other Monster first!");
-            }
-
+    public void attackDragonBoss(){
+        if (isBanditDefeated()) {
+            System.out.println("Congratulations! You defeat the DragonBoss! You win the game!");
+            maze.removeDragonBossFromMaze();
+        } else {
+            System.out.println("You need defeat other Monster first!");
         }
 
-
     }
+
+
+}
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         DungeonGame dungeonGame = new DungeonGame();
         dungeonGame.startGame();
     }
